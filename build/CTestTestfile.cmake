61c53f39f1416907aa3c669265e5df786508d071
yuichi okugawa <okugawa@lx2.lal.in2p3.fr>
2018-09-22 14:33:52 +0200

    sorting

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /users/ilc/okugawa/QQbarAnalysis
-# Build directory: /users/ilc/okugawa/QQbarAnalysis/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
