116022de7d4c2f0e2ac3e308de49178f7ebe963f
irles <irles@lal.in2p3.fr>
2019-03-13 18:39:02 +0100

    Modification in QQbarProcessor, BBBarProcessor and VertexChargeOperator in order to use the Kaon ID (and event shape variables). Also some minor code cleaning done.

--- include/BBbarAnalysis.hh
+++ include/BBbarAnalysis.hh
@@ -14,6 +14,7 @@
 #include <UTIL/LCRelationNavigator.h>
 #include <UTIL/PIDHandler.h>
 #include <IMPL/ReconstructedParticleImpl.h>
+#include <IMPL/LCCollectionVec.h>
 
 // ----- include for verbosity dependend logging ---------
 #include "marlin/VerbosityLevels.h"
@@ -32,38 +33,44 @@ using namespace marlin ;
 namespace QQbarProcessor 
 {
 
-	class BBbarAnalysis 
-	{
+  class BBbarAnalysis 
+  {
 	  
-	public:
+  public:
 	  
-	  BBbarAnalysis ();
-	  virtual ~BBbarAnalysis () {};
+    BBbarAnalysis ();
+    virtual ~BBbarAnalysis () {};
 	  
-	  //METHODS
+    //METHODS
 	  
-	  void Init(std::string _hfilename) ;
-	  std::vector< MCParticle * > AnalyseGeneratorBBbar(QQbarMCOperator & opera);
-	  void AnalyseBBbar(LCEvent * evt, 
-			    std::string _colName ,
-			    std::string _JetsColName ,
-			    std::string _JetsRelColName ,
-			    std::string _MCColName ,
-			    std::string _colRelName  ) ;
+    void Init(std::string _hfilename) ;
+    std::vector< MCParticle * > AnalyseGeneratorBBbar(QQbarMCOperator & opera);
+    void AnalyseGeneratorBBbar_PS(QQbarMCOperator & opera);
+    void AnalyseBBbar(LCEvent * evt,
+		      bool _boolDBDanalysis,
+		      bool _kaoncheat,
+		      std::string _colRelName,
+		      std::string _colName ,
+		      std::string _initialJetsColName,
+		      std::string _JetsColName ,
+		      std::string _JetsRelColName ,
+		      std::string _MCColName,
+		      std::string _KaonTaggerName
+		      ) ;
 
-	  void ClearVariables();
-	  void End();
+    void ClearVariables();
+    void End();
     
 		    
-	private:
+  private:
 
-	  void MatchB(std::vector<RecoJet*> * bjets, std::vector< MCParticle * > & mcbs, LCCollection * mcvtxcol);
+    void MatchB(std::vector<RecoJet*> * bjets, std::vector< MCParticle * > & mcbs, LCCollection * mcvtxcol);
 
-	  TFile * _hfile;
-	  TTree * _hTree;
-	  StatsData_BBbar _stats;
+    TFile * _hfile;
+    TTree * _hTree;
+    StatsData_BBbar _stats;
 
-	} ;
+  } ;
 		
 } /* TTbarAnalisys */
 
