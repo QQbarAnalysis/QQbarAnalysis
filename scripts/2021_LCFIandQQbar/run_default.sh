6f5fedc0f74f55ad4c0be1797df3b3d290db5c95
airqui <airqui@ific.uv.es>
2021-02-24 19:11:17 +0100

    xml scripts for running of the processor. Version 1

---
+++ scripts/2021_LCFIandQQbar/run_default.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# file name: kt_xNAMEfile.sh
+
+path="/lhome/ific/a/airqui/QQbar/condor/E250-SetA/250GeV_eL_norestorer_lcfiplus"
+source $path/init_ilcsoft.sh
+cp -r ${path}/data $PWD/.
+cp -r ${path}/lib $PWD/.
+cp -r ${path}/lcfiweights $PWD/.
+cp ${path}/GearOutput.xml $PWD/.
+cp ${path}/test_xPROD_xNAMEfile.xml .
+
+export MARLIN_DLL="$MARLIN_DLL:$PWD/lib/libQQbarProcessor.so"
+export MARLIN_DLL="$MARLIN_DLL:$PWD/lib/libReconstructedParticleImpl_CopyProcessor.so"
+echo $PWD
+Marlin ${PWD}/test_xPROD_xNAMEfile.xml
--- scripts/test_y23/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export MARLIN_DLL="$MARLIN_DLL:$PWD/lib/libQQbarProcessor.so"
-#export MARLIN_DLL="$MARLIN_DLL:$PWD/lib/libVertexRestorer.so"
-export MARLIN_DLL="$MARLIN_DLL:$PWD/lib/libmyFastJetProcessor.so"
-
-Marlin test_7637_00001.xml
