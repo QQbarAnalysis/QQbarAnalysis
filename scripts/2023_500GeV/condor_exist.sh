3bdd0cf904e6dc68656f5b03cdb23668fd8de657
A. Irles <adrian.irles@ific.uv.es>
2023-01-26 12:26:18 +0100

    scripts to launch the 500 GeV analysis using condor

--- scripts/2023_500GeV/condor_exist.sh
+++ scripts/2023_500GeV/condor_exist.sh
@@ -4,49 +4,82 @@ path=$PWD
 
 for prod in "eL_pR" "eR_pL"
 do
-    folder_name="/lustre/ific.uv.es/prj/ific/flc/mc-opt-3/"${prod}"/"
+    folder_name="/lustre/ific.uv.es/prj/ific/flc/500-TDR_ws/2f_Z_hadronic/ILD_l5_o1_v02/v02-02-03/"
     cd $folder_name
     echo $folder_name
     
-    FILES=*
-    
+    subfolders=*
+	prod2="eR.pL"
+    if [[ $prod == "eL_pR" ]]; then
+  		prod2="eL.pR"
+	fi
+
     i=0
-    for f in $FILES
+    for sf in $subfolders
     do
-	name="00"$i
-	if [ $i -gt 9 ]; then
-            name="0"$i
-	fi
-	
-	if [ $i -gt 99 ]; then
-            name=$i
-	fi
-	if [ -f ${path}/${prod}_${name}.root ];
-        then
-            echo "Skip "${name}
-        else
-	    cp ${path}/test_default.xml ${path}/test_${prod}_${name}.xml
-            sed -i -e 's/xPROD/'${prod}'/g' ${path}/test_${prod}_${name}.xml
-	    sed -i -e 's/NFile/'${f}'/g' ${path}/test_${prod}_${name}.xml
-	    sed -i -e 's/newFileN/'${name}'/g' ${path}/test_${prod}_${name}.xml
-	    
-	    cp ${path}/run_default.sh ${path}/run_${prod}_${name}.sh
-	    sed -i -e 's/xPROD/'${prod}'/g' ${path}/run_${prod}_${name}.sh
-	    sed -i -e 's/xNAMEfile/'${name}'/g' ${path}/run_${prod}_${name}.sh
-	    cp ${path}/run_default.sub ${path}/run_${prod}_${name}.sub
-            sed -i -e 's/xPROD/'${prod}'/g' ${path}/run_${prod}_${name}.sub
-	    sed -i -e 's/xNAMEfile/'${name}'/g' ${path}/run_${prod}_${name}.sub
-	    
-	    echo "Submit --- > " ${name}   
-	    cd -
-	    condor_submit run_${prod}_${name}.sub
-	    #sleep 0.1s 
-	    cd -
-	fi    
-	#rm ${path}/run_${prod}_${name}.sub
-	#rm ${path}/run_${prod}_${name}.sh
-	i=$((i+1))
-    done
-    cd -
+
+		echo ${folder_name}"/"${sf}"/000/*"${prod2}"*"
+		if [ ! -f ${folder_name}/${sf}/000/*${prod2}* ]; then
+			echo "Skip "${sf} "because has no "${prod2}
+			continue
+		fi
+
+		#cd ${folder_name}/${sf}/000/
+		cd ${folder_name}/${sf}/000/
+		echo " tessssst   -> " $PWD
+		filenames=*
+		
+		for fileN in $filenames
+    	do
+			echo $fileN
+			name="0000"$i
+			if [ $i -gt 9 ]; then
+				name="000"$i
+			fi
+			if [ $i -gt 99 ]; then
+				name="00"$i
+			fi
+			if [ $i -gt 999 ]; then
+				name="0"$i
+			fi
+			if [ $i -gt 9999 ]; then
+				name=$i
+			fi
+			if [ -f /lustre/ific.uv.es/prj/ific/flc/ntuples-2021/2f_hadronic_sample/unmerged_${prod}_${name}.root ] || [ -f /lustre/ific.uv.es/prj/ific/flc/ntuples-2021/2f_hadronic/unmerged_${prod}_${name}.root ];	then
+				echo "Skip "${name}
+			else
+				cd ${path}
+				cp ${path}/test_default.xml ${path}/test_${prod}_${name}.xml
+				sed -i -e 's/LCIOFOLDER/'${sf}'/g' ${path}/test_${prod}_${name}.xml
+				sed -i -e 's/LCIOFILE/'${fileN}'/g' ${path}/test_${prod}_${name}.xml
+				sed -i -e 's/newFileN/'${name}'/g' ${path}/test_${prod}_${name}.xml
+				if [ $i -gt 200 ]; then
+					sed -i -e 's/xTYPEANALYSIS/'1'/g' ${path}/test_${prod}_${name}.xml
+					sed -i -e 's/xFOLDER/'2f_hadronic_${prod}'/g' ${path}/test_${prod}_${name}.xml
+					sed -i -e 's/xPROD/'${prod}'/g' ${path}/test_${prod}_${name}.xml
+				else 
+					sed -i -e 's/xTYPEANALYSIS/'0'/g' ${path}/test_${prod}_${name}.xml
+					sed -i -e 's/xFOLDER/'2f_hadronic_sample_${prod}'/g' ${path}/test_${prod}_${name}.xml
+					sed -i -e 's/xPROD/'${prod}'/g' ${path}/test_${prod}_${name}.xml
+				fi
+
+
+				cp ${path}/run_default.sh ${path}/run_${prod}_${name}.sh
+				sed -i -e 's/xPROD/'${prod}'/g' ${path}/run_${prod}_${name}.sh
+				sed -i -e 's/xNAMEfile/'${name}'/g' ${path}/run_${prod}_${name}.sh
+				cp ${path}/run_default.sub ${path}/run_${prod}_${name}.sub
+				sed -i -e 's/xPROD/'${prod}'/g' ${path}/run_${prod}_${name}.sub
+				sed -i -e 's/xNAMEfile/'${name}'/g' ${path}/run_${prod}_${name}.sub
+						
+				condor_submit run_${prod}_${name}.sub
+				#sleep 0.1s 
+				cd -
+			fi
+		
+			#rm ${path}/run_${prod}_${name}.sub
+			#rm ${path}/run_${prod}_${name}.sh
+			i=$((i+1))
+		done
+	done
 done
 
--- scripts/2023_500GeV/run_default.sh
+++ scripts/2023_500GeV/run_default.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # file name: kt_xNAMEfile.sh
 
-path="/lhome/ific/a/airqui/QQbar/QQbarAnalysis-500GeV/scripts/2021_500GeV"
+path="/lhome/ific/a/airqui/QQbar/QQbarAnalysis-500GeV/scripts/2023_500GeV"
 source $path/init_ilcsoft.sh
 cp -r ${path}/data $PWD/.
 cp -r ${path}/lib $PWD/.
@@ -12,3 +12,14 @@ cp ${path}/test_xPROD_xNAMEfile.xml .
 export MARLIN_DLL="$MARLIN_DLL:$PWD/lib/libQQbarProcessor.so"
 echo $PWD
 Marlin ${PWD}/test_xPROD_xNAMEfile.xml
+#if [ -f ${path}/2f_hadronic_xPROD_xNAMEfile.root ];	then
+#    mv ${path}/2f_hadronic_xPROD_xNAMEfile.root /lustre/ific.uv.es/prj/ific/flc/ntuples-2021/500GeV_2023_signal/.
+#fi
+#if [ -f ${path}/2f_hadronic_sample_xPROD_xNAMEfile.root ];	then
+#    mv ${path}/2f_hadronic_sample_xPROD_xNAMEfile.root /lustre/ific.uv.es/prj/ific/flc/ntuples-2021/500GeV_2023_signal/.
+#fi
+#rm ${path}/*xPROD_xNAMEfile*log
+#rm ${path}/*xPROD_xNAMEfile*sub
+#rm ${path}/*xPROD_xNAMEfile*txt
+#rm ${path}/*xPROD_xNAMEfile*sh
+#rm ${path}/*xPROD_xNAMEfile*txt
